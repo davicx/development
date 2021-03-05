4bb045bc13a293f55d9e36265d098991f9959eb8
vasquez_d <vasquezd@oregonstate.edu>
2021-03-04 17:46:24 -0800

    updates

--- leetcode/java/src/main/java/Main.java
+++ leetcode/java/src/main/java/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
+        int[] nums = {2,3,1,1,4};
+        canJump(nums);
+
     }
+
+    public static boolean canJump(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+
+
+        return true;
+    }
+
 }
