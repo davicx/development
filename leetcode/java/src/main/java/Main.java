09b5599997da4044aa1d26a75cd3d5a500c14dc8
vasquez_d <vasquezd@oregonstate.edu>
2021-03-16 16:09:51 -0700

    updates

--- leetcode/java/src/main/java/Main.java
+++ leetcode/java/src/main/java/Main.java
@@ -2,19 +2,33 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
-        int[] nums = {2,3,1,1,4};
-        canJump(nums);
+        //int[] height = {4,3,2,1,4};
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
 
     }
 
-    public static boolean canJump(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
+    public static int maxArea(int[] height) {
+        int maxVolume = 0;
+        int currentVolume = 0;
+        int distance;
+        int lowerWall;
+
+        for(int i = 0; i < height.length; i++) {
+            for(int j = i + 1; j < height.length; j++) {
+                distance = j - i;
+                lowerWall = Math.min(height[i], height[j]);
+                //System.out.println(height[i] + " " + height[j] + " " + lowerWall);
+                System.out.println(distance + " " + lowerWall);
+
+                currentVolume = lowerWall * distance;
+                maxVolume = Math.max(maxVolume, currentVolume);
+                //System.out.println(currentVolume);
+
+            }
         }
+        return maxVolume;
 
-
-        return true;
     }
 
 }
