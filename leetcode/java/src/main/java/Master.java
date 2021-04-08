df99a1480af028944a04383495126845b24b3ec7
vasquez_d <vasquezd@oregonstate.edu>
2021-04-08 14:41:26 -0700

    updates

--- leetcode/java/src/main/java/Master.java
+++ leetcode/java/src/main/java/Master.java
@@ -1,4 +1,13 @@
 public class Master {
+    public static void main(String[] args) {
+        System.out.println("hi");
 
-    
+        User currentUser = new User(1, "Davey");
+        User userFriend = new User(2, "Bilbo");
+        //boolean result = object1.getClass().equals( object2.getClass());
+        System.out.println(currentUser.userName);
+
+
+    }
+
 }
