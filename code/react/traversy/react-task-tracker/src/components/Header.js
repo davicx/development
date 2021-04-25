e1bad5588af9db1365db5c29283dd6ee8f11d2ae
vasquez_d <vasquezd@oregonstate.edu>
2021-04-24 17:22:15 -0700

    updates

---
+++ code/react/traversy/react-task-tracker/src/components/Header.js
@@ -0,0 +1,131 @@
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
+    return (
+        <div>
+            <p style={headingStyle}> style </p>
+            <p> { title } </p>
+        </div>
+    )
+}
+
+Header.defaultProps = {
+  title: 'Hiya!'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+   color: 'blue',
+}
+
+export default Header
+
+//TYPE 1:
+/*
+const Header = (props) => {
+    return (
+        <div>
+            <p>{props.hiya}</p>
+        </div>
+    )
+}
+
+
+Header.defaultProps = {
+  hiya: 'Hiya!'
+}
+
+export default Header
+ */
+
+/*
+//TYPE 2: Props Input
+//App.js
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="container">
+      <header className="App-header">
+        <p> hi </p>
+        <p> { console.log("hiya") }</p>
+        <Header />
+         <Header title = 'Hello'/>  
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+//Header 
+const Header = (props) => {
+  return (
+      <div>
+          <p>task tracker {props.title}</p>
+      </div>
+  )
+}
+
+export default Header
+*/
+
+//TYPE 3: Default Props
+/*
+const Header = (props) => {
+    return (
+        <div>
+            <p>{props.hiya}</p>
+        </div>
+    )
+}
+
+Header.defaultProps = {
+  hiya: 'Hiya!'
+}
+
+export default Header
+
+*/
+
+/*
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
+
+export default Header
+*/
