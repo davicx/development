9e3fa3bdbb23a021ef8ed728809dc16c2a410785
vasquez_d <vasquezd@oregonstate.edu>
2021-05-07 15:08:25 -0700

    updates

---
+++ leetcode/javascript/js/maximumSubarray.js
@@ -0,0 +1,10 @@
+//Two Pointer Array
+let nums = [5,4,-1,7,8];
+maxSubArray(nums);
+
+function maxSubArray(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        console.log(nums[i]);
+    }
+}
+
