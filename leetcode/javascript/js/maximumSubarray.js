68aaa067c988682a257fe402cb79c2dd6484013b
vasquez_d <vasquezd@oregonstate.edu>
2021-05-08 16:58:07 -0700

    updates

--- leetcode/javascript/js/maximumSubarray.js
+++ leetcode/javascript/js/maximumSubarray.js
@@ -1,10 +1,30 @@
 //Two Pointer Array
-let nums = [5,4,-1,7,8];
-maxSubArray(nums);
+//let nums = [-2,2,5,-11,6];
+//let nums = [1,2,3,4];
+let nums = [-1];
+
+let maxSum = maxSubArray(nums);
+console.log("ANSWER " + maxSum);
+
 
 function maxSubArray(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        console.log(nums[i]);
+    var maxSum = -Number.MAX_VALUE;
+    let currentSum = 0;
+
+    for (let leftPointer = 0; leftPointer < nums.length; leftPointer++) {
+        console.log("Loop Start " + maxSum);
+        for (let rightPointer = leftPointer; rightPointer < nums.length; rightPointer++) {
+            currentSum = currentSum + nums[rightPointer];
+            //console.log(nums[leftPointer] + " " + nums[rightPointer]);
+            
+            maxSum = Math.max(maxSum, currentSum);
+            console.log("Max " + maxSum + " Current " + currentSum);
+        }
+        //console.log("");
+        currentSum = 0;
+        
     }
+    return maxSum;
 }
 
+//console.log(Math.max(Number.MIN_VALUE + 1000, -1));
