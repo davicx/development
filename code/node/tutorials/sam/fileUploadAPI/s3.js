0c1564ac73eabd9b7a153a49d70342e7ce92a334
David <davidcharlesv@gmail.com>
2024-03-23 16:15:57 -0700

    updates

---
+++ code/node/tutorials/sam/fileUploadAPI/s3.js
@@ -0,0 +1,68 @@
+require('dotenv').config()
+//console.log(process.env)
+const S3 = require('aws-sdk/clients/s3')
+const fs = require('fs');
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey
+  })
+
+/*  
+const s3 = new S3({
+    region: region,
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey
+  })
+*/
+
+//Upload to S3
+function uploadFile(file) {
+    const fileStream = fs.createReadStream(file.path)
+    //const key = "images/" + file.filename;
+    const key = "images/" + file.filename;
+  
+    const uploadParams = {
+      Bucket: bucketName,                                                                                                                                                               
+      Body: fileStream,
+      Key: key
+    }
+  
+    return s3.upload(uploadParams).promise()
+
+}
+
+exports.uploadFile = uploadFile
+
+//Download from S3
+function getFileStream(fileKey) {
+  const downloadParams = {
+    Key: fileKey,
+    Bucket: bucketName
+  }
+
+  return s3.getObject(downloadParams).createReadStream()
+}
+
+exports.getFileStream = getFileStream
+
+
+
+/*
+//Download from S3
+function getFileStream(fileKey) {
+const downloadParams = {
+    Key: fileKey,
+    Bucket: bucketName
+}
+
+return s3.getObject(downloadParams).createReadStream()
+}
+exports.getFileStream = getFileStream
+  */
